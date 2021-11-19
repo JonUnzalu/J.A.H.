2359228d3e99082d5096d54902b5d0a79e9ce966
JonUnzalu <jonunzalu@yahoo.es>
2021-11-19 10:51:07 +0100

    poner el about en jquery :)

--- proyectosDjango/JatetxeaProyecto/jatetxea/static/about.js
+++ proyectosDjango/JatetxeaProyecto/jatetxea/static/about.js
@@ -1,9 +1,52 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    
+    var kopuruak = new Array()
+    var cantidadComidas = document.querySelectorAll('.kopuruak').length;
+
+    //Pillamos el id de la primera comida de la página :)
+    var idPrimeraComida = parseInt(document.querySelectorAll(".janariId")[0].innerHTML);
+
+    if (document.querySelectorAll('.usuarioNombreLogado').length > 0) {
+        document.querySelectorAll(".kopuruak").forEach(a => a.style.display = "initial");
+
+        if (localStorage.getItem("estadoKarrito") !== null && localStorage.getItem("estadoKarrito").length > 0) {
+            kopuruak = [];
+            kopuruak = JSON.parse(localStorage.getItem("estadoKarrito"));
+
+            for (let i = 1; i < cantidadComidas + 1; i++) {
+
+                document.getElementById("kopurua" + idPrimeraComida).innerHTML = kopuruak[idPrimeraComida - 1].kopurua;
+                idPrimeraComida++;
+            }
+        }
+        else {
+            if (kopuruak.length > 0) {
+            }
+            else {
+                for (let i = 1; i < cantidadComidas + 1; i++) {
+                    const kopuruxJanari = {
+                        kopurua: 0,
+                        kopuruMax: 20,
+                        prezioa: document.getElementById("prezioa" + i).innerHTML,
+                        idJanari: i,
+                        desJanari: document.getElementById("janari" + i).innerHTML
+                    }
+                    kopuruak.push(kopuruxJanari)
+                    document.getElementById("kopurua" + i).innerHTML = kopuruak[i - 1].kopurua;
+                }
+                localStorage.setItem("estadoKarrito", JSON.stringify(kopuruak));
+            }
+        }
+    }
+    else {
+        document.querySelectorAll(".kopuruak").forEach(a => a.style.display = "none");
+    }
+
 
     $(".substractImagen").click(function () {
         var stringId = $(this).attr('janariId');
-        var id = parseInt(stringId);        
-        
+        var id = parseInt(stringId);
+
         subtractFood(id);
     });
 
@@ -13,65 +56,22 @@ $(document).ready(function() {
 
         addFood(id);
     });
-});
 
-//ABOUT.HTML
-var kopuruak = new Array()
-var cantidadComidas = document.querySelectorAll('.kopuruak').length;
+    function addFood(codJanaria) {
+        if (kopuruak[codJanaria - 1].kopurua < kopuruak[codJanaria - 1].kopuruMax) {
+            kopuruak[codJanaria - 1].kopurua = kopuruak[codJanaria - 1].kopurua + 1;
+            document.getElementById("kopurua" + codJanaria).innerHTML = kopuruak[codJanaria - 1].kopurua;
 
-//Pillamos el id de la primera comida de la página :)
-var idPrimeraComida = parseInt(document.querySelectorAll(".janariId")[0].innerHTML);     
-
-if(document.querySelectorAll('.usuarioNombreLogado').length>0) {
-    document.querySelectorAll(".kopuruak").forEach(a=>a.style.display = "initial");
-
-    if (localStorage.getItem("estadoKarrito") !== null && localStorage.getItem("estadoKarrito").length > 0) {
-        kopuruak = [];
-        kopuruak = JSON.parse(localStorage.getItem("estadoKarrito"));
-    
-        for (let i = 1; i < cantidadComidas+1; i++) {
-    
-            document.getElementById("kopurua" + idPrimeraComida).innerHTML = kopuruak[idPrimeraComida-1].kopurua;
-            idPrimeraComida++;
-        }
-    }
-    else{
-        if (kopuruak.length > 0) {
-        }
-        else {
-            for (let i = 1; i < cantidadComidas+1; i++) {
-                const kopuruxJanari = {
-                    kopurua: 0,
-                    kopuruMax: 20,
-                    prezioa: document.getElementById("prezioa" + i).innerHTML,
-                    idJanari: i,
-                    desJanari: document.getElementById("janari" + i).innerHTML
-                }
-                kopuruak.push(kopuruxJanari)
-                document.getElementById("kopurua" + i).innerHTML = kopuruak[i-1].kopurua;
-            }
             localStorage.setItem("estadoKarrito", JSON.stringify(kopuruak));
         }
     }
-}
-else{
-    document.querySelectorAll(".kopuruak").forEach(a=>a.style.display = "none");
-}
 
-function addFood(codJanaria) {
-    if (kopuruak[codJanaria-1].kopurua < kopuruak[codJanaria-1].kopuruMax) {
-        kopuruak[codJanaria-1].kopurua = kopuruak[codJanaria-1].kopurua + 1;
-        document.getElementById("kopurua" + codJanaria).innerHTML = kopuruak[codJanaria-1].kopurua;
+    function subtractFood(codJanaria) {
+        if (kopuruak[codJanaria - 1].kopurua > 0) {
+            kopuruak[codJanaria - 1].kopurua = kopuruak[codJanaria - 1].kopurua - 1;
+            document.getElementById("kopurua" + codJanaria).innerHTML = kopuruak[codJanaria - 1].kopurua;
 
-        localStorage.setItem("estadoKarrito", JSON.stringify(kopuruak));
+            localStorage.setItem("estadoKarrito", JSON.stringify(kopuruak));
+        }
     }
-}
-
-function subtractFood(codJanaria) {
-    if (kopuruak[codJanaria-1].kopurua > 0) {
-        kopuruak[codJanaria-1].kopurua = kopuruak[codJanaria-1].kopurua - 1;
-        document.getElementById("kopurua" + codJanaria).innerHTML = kopuruak[codJanaria-1].kopurua;
-
-        localStorage.setItem("estadoKarrito", JSON.stringify(kopuruak));
-    }
-}
+});
