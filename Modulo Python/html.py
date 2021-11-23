ec8df844847e1b7e15d13e99bd3dc447847dc31d
JonUnzalu <jonunzalu@yahoo.es>
2021-11-23 19:12:57 +0100

    borrar un par de cosas

--- Modulo Python/html.py
+++
@@ -1,3 +0,0 @@
-import webbrowser
-url = 'http://192.168.73.13:8080'
-webbrowser.open(url, new=2)  # open in new tab
--- probak/JatetxeaProyecto/jatetxea/templates/insertjanarimota.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO jatetxea_janarimota (izena, argazkia) VALUES 
-('Kebab', 'produktuakBg1.jpg'), 
-('Hamburgesa', 'burgerBack.jpg'),
-('Pizza', 'pizzaback.jpg');
